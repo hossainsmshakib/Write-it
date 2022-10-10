b20fb0747413703a946d76fe9bc7527175c45ea0
sm-shakib <smsakib2403@gmail.com>
2022-10-10 22:19:27 +0600

    end

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const articleRouter = require('./routes/articles')
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
 const app = express();
 
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.use('/articles', articleRouter)
-
-app.get("/", (req, res) => {
-    const articles = [{
-        title: "Test Article",
-        createdAt: Date.now(),
-        description: "Test Description"
-    }]
-  res.render('index2', { articles: 'Hello'});
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
